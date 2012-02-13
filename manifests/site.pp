588f84067ed34152d68339e27cbdc3b5deeda8c6
root <root@polk.npulsetech.com>
2012-02-13 11:53:28 -0500

    initial commit

---
+++ manifests/site.pp
@@ -0,0 +1,15 @@
+# Create "/tmp/testfile" if it doesn't exist.
+class test_class {
+    file { "/tmp/testfile":
+       ensure => present,
+       mode   => 644,
+       owner  => root,
+       group  => root
+    }
+}
+
+# tell puppet on which client to run the class
+node default {
+    include test_class
+}
+
